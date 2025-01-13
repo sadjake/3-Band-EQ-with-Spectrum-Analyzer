684a4a56a24fdab34cc699af43665feb05404195
sadjake <jakeyoonchoi@gmail.com>
2025-01-13 01:22:11 -0800

    Initialize lowcut frequency

--- Source/PluginProcessor.cpp
+++ Source/PluginProcessor.cpp
@@ -186,7 +186,14 @@ void SimpleEQAudioProcessor::setStateInformation (const void* data, int sizeInBy
 juce::AudioProcessorValueTreeState::ParameterLayout
     SimpleEQAudioProcessor::createParameterLayout()
 {
+        juce::AudioProcessorValueTreeState::ParameterLayout layout;
         
+        layout.add(std::make_unique<juce::AudioParameterFloat>("LowCut Freq",
+                                                               "LowCut Freq",
+                                                               juce::NormalisableRange<float>(20.f, 20000.f, 1.f, 1.f),
+                                                               20.f)); 
+        
+        return layout;
 }
 
 //==============================================================================
